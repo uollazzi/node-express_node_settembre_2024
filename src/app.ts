1f86f7a848a61bacc3c0df6cc3e21cf39a69a878
Matteo Valenzi <dilloawallace@hotmail.com>
2024-09-16 21:11:16 +0200

    initial

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("App Works!");
+import express, { Request, Response } from "express";
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("<h1>Ciao!</h1>");
+});
+
+app.listen(port, () => {
+    console.log(`Server in esecuzione su http://localhost:${port}`);
+    console.log("Premere CTRL+C per arrestare.")
+})
